951b82eb19436058912e82677f5cc1081cc00091
Mike89745 <mikes89745@gmail.com>
2018-06-05 10:26:57 +0200

    init

--- MouseSpeedChanger/MainWindow.xaml.cs
+++ MouseSpeedChanger/MainWindow.xaml.cs
@@ -12,17 +12,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Runtime.InteropServices;
 namespace MouseSpeedChanger
 {
-    /// <summary>
-    /// Interakční logika pro MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        public const UInt32 SPI_SETMOUSESPEED = 0x0071;
+        [DllImport("User32.dll")]
+        static extern Boolean SystemParametersInfo(
+            UInt32 uiAction,
+            UInt32 uiParam,
+            UInt32 pvParam,
+            UInt32 fWinIni);
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        public static void SetMouseSpeed(int intSpeed)
+        {
+            uint ptr = Convert.ToUInt32(intSpeed);
+            SystemParametersInfo(SPI_SETMOUSESPEED, 0, ptr, 0);
+        }
+        private void SlowMouseSpeed(object sender, RoutedEventArgs e)
+        {
+            SetMouseSpeed(5);
+        }
+
+        private void DefaultMouseSpeed(object sender, RoutedEventArgs e)
+        {
+            SetMouseSpeed(1);
+        }
+
+        private void FastMouseSpeed(object sender, RoutedEventArgs e)
+        {
+            SetMouseSpeed(15);
         }
     }
 }
